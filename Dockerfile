30bda9d1986636fa38288a4825e96ef07bc06ffd
chupacabramiamor <chupacabramiamor@gmail.com>
2021-10-04 12:40:09 +0300

    init

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM phpearth/php:7.3-nginx
+
+ARG PUID=1000
+ARG PGID=1000
+
+RUN apk update && \
+    apk add curl npm yarn git openssh-client sqlite php7.3-sqlite3 php7.3-pdo_sqlite && \
+    curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer --1 && \
+    apk --no-cache add shadow &&\
+    /usr/sbin/usermod -u $PUID www-data && /usr/sbin/groupmod -g $PGID www-data &&\
+    chown www-data:www-data /var/www
+
+RUN npm install -g gulp
+
+COPY ./docker/default.conf /etc/nginx/conf.d/default.conf
+
+WORKDIR /var/www/html/htdocs
---
+++ up.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+DOCKER_HTTP_PORT=$(grep DOCKER_HTTP_PORT docker/.env | cut -d '=' -f 2-)
+CLIENT_NAME=$(grep CLIENT_NAME docker/.env | cut -d '=' -f 2-)
+PROJECT_NAME=$(grep PROJECT_NAME docker/.env | cut -d '=' -f 2-)
+DN="${PROJECT_NAME}.${CLIENT_NAME}"
+CONTAINER_NAME="${CLIENT_NAME}.${PROJECT_NAME}"
+NGINX_CONF="/etc/nginx/sites-available/$CLIENT_NAME"
+
+if [ ! -f $NGINX_CONF ]; then
+    sudo touch $NGINX_CONF
+fi
+
+# cat docker/nginx.stub | sudo tee -a $NGINX_CONF
+# service nginx restart
+
+if [[ -z $(grep "${DN}" /etc/hosts) ]]; then
+    echo "127.0.0.1 ${DN}" | sudo tee -a /etc/hosts
+fi
+
+# docker build . -t lead9-nginx-php73 --build-arg PUID=$(id -u) --build-arg PGID=$(id -g)
+docker build . -t lead9-nginx-php73 --build-arg PUID=$(id -u)
+docker create -p=$DOCKER_HTTP_PORT:80 -v=$(pwd):/var/www/html --name=$CONTAINER_NAME lead9-nginx-php73
+docker start $CONTAINER_NAME
+
+# Вход в контейнер
+# docker exec -it -u=$(id -u) $CONTAINER_NAME sh
